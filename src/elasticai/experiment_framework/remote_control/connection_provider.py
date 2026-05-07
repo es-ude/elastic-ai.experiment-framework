0d6ef83565cbe769847aa391eced5c47c78e4d39
Schoolher <keuwohermann@gmail.com>
2026-05-07 22:16:41 +0200

    feat!: add new function call flow of python protocol with sockets+asyncio
    replaces the old version already
    
    BREAKING CHANGE
    
    
    fix: restoring important deleted files
    
    ref:task refactoring
    
    ref:clean device management and sessions
    
    fix: folders restructuring
    
    ref: changing the interaction between task and task manager producer/consumer
    
    ref: refinining units and system tests
    
    fix: removing is_last logic

---
+++ src/elasticai/experiment_framework/remote_control/connection_provider.py
@@ -0,0 +1,42 @@
+import asyncio
+import logging
+
+from .constants import (
+    NUM_MAX_RETRIES,
+    RETRY_DELAY_SECONDS,
+)
+from .io_stream import IOStream
+from .tcp_protocol_stream import TCPProtocolStream
+
+_logger = logging.getLogger(__name__)
+
+
+class ConnectionProvider:
+    def __init__(self, max_trials: int = NUM_MAX_RETRIES):
+        self._max_trials = max_trials
+
+    async def connectTCP(
+        self, host: str, port: int, max_trials: int | None = None
+    ) -> IOStream:
+        if max_trials is not None:
+            self._max_trials = max_trials
+        loop = asyncio.get_running_loop()
+
+        for attempt in range(self._max_trials):
+            try:
+
+                def factory():
+                    return TCPProtocolStream(self._on_lost)
+
+                _, _protocol = await loop.create_connection(factory, host, port)
+                _logger.debug("[CLIENT] connected via TCP")
+
+                return _protocol
+            except Exception as e:
+                _logger.error(f"[CLIENT] attempt {attempt + 1} failed: {e}")
+                await asyncio.sleep(RETRY_DELAY_SECONDS)
+        _logger.error(f"[CLIENT] fail to connect after {self._max_trials}:")
+        raise ConnectionError(f"[CLIENT] fail to connect after {self._max_trials}")
+
+    def _on_lost(self, exc):
+        _logger.warning(f"connection lost: {exc}")
---
+++ src/elasticai/experiment_framework/remote_control/exceptions.py
@@ -0,0 +1,18 @@
+class ProtocolError(Exception):
+    pass
+
+
+class UnexpectedMessageError(ProtocolError):
+    pass
+
+
+class InvalidHeaderError(ProtocolError):
+    pass
+
+
+class DeviceAlreadyConnectedError(ProtocolError):
+    pass
+
+
+class DeviceNotFoundError(ProtocolError):
+    pass
