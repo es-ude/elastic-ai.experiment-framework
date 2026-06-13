f3451b2dadb605a2ce3a6e442711e860569fd801
Timon Hüsemann <tihues@outlook.com>
2026-06-13 20:45:32 +0200

    feat: add receiver and msg handler
    - receiver acts as entry point for incoming messages

---
+++ example-firmwares/remote_control/src/lib/msg_handler.h
@@ -0,0 +1,10 @@
+#include "frame.h"
+#include "ringbuffer.h"
+#include "task_manager.h"
+
+int msg_open_task(Frame *frame, TaskManager *task_manager);
+int msg_close_task(Frame *frame, TaskManager *task_manager);
+int msg_return(Frame *frame);
+int msg_data_chunk(RingBuffer *task_rb, Frame *frame, TaskManager *task_manager);
+
+void handle_incoming_frame(RingBuffer *task_rb, Frame *frame, TaskManager *task_manager);
