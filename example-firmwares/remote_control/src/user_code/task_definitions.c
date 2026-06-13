dc288b21046507eabd6b09c09c39b21c2cec6bb5
Timon Hüsemann <tihues@outlook.com>
2026-06-13 20:49:59 +0200

    feat: add Task Managment System
    - internal core loop for the embedded side. can start tasks, processes them and can stop them
    
    
    feat: add embedded functions
    
    - adds the embedded functions, which are the contact point where embedded programmer interacts with the tasks

---
+++ example-firmwares/remote_control/src/user_code/task_definitions.c
@@ -0,0 +1,56 @@
+#include "task_definitions.h"
+#include "msg_types.h"
+#include "frame_builder.h"
+
+#include <stdio.h>
+#include <string.h>
+
+void default_setup(TaskContext *task_context)
+{
+}
+
+void default_teardown(TaskContext *task_context)
+{
+}
+
+void send_mirror_reply(TaskContext *task_context)
+{
+    memcpy(task_context->output_data, task_context->input_data, task_context->input_data_len);
+    task_context->output_data_len = task_context->input_data_len;
+
+    task_context->task_services.send_data(&task_context->task_services, 0, task_context->output_data, task_context->output_data_len);
+    task_context->task_services.send_return(&task_context->task_services, 0, 0);
+}
+
+void func1(TaskContext *task_context)
+{
+    char *msg = "func1 called";
+}
+
+// Contains the functions that should be callable from the client_protocol via execute_function,
+// position determines function_id
+
+static TaskDefinition task_definition_table[] = {
+    {.setup = default_setup,
+     .tear_down = default_teardown,
+     .handle = send_mirror_reply},
+    {.setup = default_setup,
+     .tear_down = default_teardown,
+     .handle = func1}};
+
+static UserTasks user_task_defintions = {.task_definitions = task_definition_table,
+                                         .size = sizeof(task_definition_table)};
+
+TaskDefinition *get_task_definition(uint32_t task_definition_id)
+{
+    if (user_task_defintions.size == 0)
+    {
+        return NULL;
+    }
+
+    if (task_definition_id < 0 || task_definition_id >= user_task_defintions.size / sizeof(TaskDefinition))
+    {
+        return NULL;
+    }
+    return &user_task_defintions.task_definitions[task_definition_id];
+}
