69eb8ff1fa27b37957bc0cfe3655bff4448d10f7
Timon Hüsemann <tihues@outlook.com>
2026-06-13 21:20:52 +0200

    feat: add main
    - adds the entry point for the whole protocol
    - adds a shell script for starting the server. important for testing

---
+++ example-firmwares/remote_control/src/app/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "ringbuffer.h"
+#include "task_manager.h"
+#include "sender.h"
+#include "receiver.h"
+#include "transport.h"
+#include "task_definitions.h"
+
+#define INCOMING_BUFFER_SIZE 128
+#define OUTGOING_BUFFER_SIZE 16
+#define TASK_BUFFER_SIZE 16
+
+static RingBuffer incoming_rb;
+static RingBuffer outgoing_rb;
+static RingBuffer task_rb;
+
+static uint8_t incoming_storage[INCOMING_BUFFER_SIZE];
+static Frame outgoing_storage[OUTGOING_BUFFER_SIZE];
+static Task *task_storage[TASK_BUFFER_SIZE];
+
+static Receiver receiver;
+static Sender sender;
+
+static Transport transport_protocol;
+
+static TaskManager task_manager;
+
+int main(int argc, char *argv[])
+{
+
+    ringbuffer_init(&incoming_rb, incoming_storage, INCOMING_BUFFER_SIZE, sizeof(uint8_t));
+    ringbuffer_init(&outgoing_rb, outgoing_storage, OUTGOING_BUFFER_SIZE, sizeof(Frame));
+    ringbuffer_init(&task_rb, task_storage, TASK_BUFFER_SIZE, sizeof(Task *));
+
+    init_task_manager(&outgoing_rb, &task_manager); // Initialize task manager with outgoing ring buffer for sending responses
+
+    TransportConfig cfg = (TransportConfig){
+        .type = TRANSPORT_SOCKET,
+        .cfg.socket.port = 8080,
+        .incoming_rb = &incoming_rb};
+
+    transport_init(&transport_protocol, cfg);
+
+    receiver = (Receiver){.incoming_rb = &incoming_rb,
+                          .transport = &transport_protocol,
+                          .task_rb = &task_rb};
+
+    sender = (Sender){
+        .transport = &transport_protocol,
+        .outgoing_rb = &outgoing_rb,
+        .msg_counter = {0}};
+
+    printf("[Server] Server was started\n");
+    fflush(stdout);
+    while (1)
+    {
+        process_rx(&receiver, &task_manager, &sender);
+
+        process_tasks(&task_rb, &outgoing_rb, &task_manager);
+
+        process_tx(&sender);
+    }
+}
